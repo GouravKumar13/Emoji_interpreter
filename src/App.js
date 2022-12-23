72d3e71ee13beeeab490d5e54c06b128a18102f5
Gourav Kumar <gouravkumarat@gmail.com>
2022-12-23 19:21:49 +0000

    added the like button using react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React, {useState} from "react";
 import "./styles.css";
 
+
 export default function App() {
+  
+  const [ likecounter, setLikeCounter ] = useState(0);
+  
+  function likeClickHandler() {
+ 
+  setLikeCounter(likecounter + 1);
+}
+
+  
+  
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Welcome </h1>
+      <button onClick={likeClickHandler}>Like me! </button>{likecounter}
     </div>
   );
 }
